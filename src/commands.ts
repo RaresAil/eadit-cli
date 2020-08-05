6759b7b293070606aafe737fa1ebc24249432e6d
RaresAil <ailincairares1@gmail.com>
2020-08-05 21:22:37 +0300

    Revert "Removed version checker"
    This reverts commit 96e51752ffa364f07fe336fa48ea5e8f21267b0c.

--- src/commands.ts
+++ src/commands.ts
@@ -3,9 +3,13 @@
 import program from 'commander';
 import fs from 'fs';
 import path from 'path';
+import colors from 'colors/safe';
+import { execSync } from 'child_process';
+
+const version = '1.0.5';
 
 program
-  .version('1.0.4')
+  .version(version)
   .description("CLI for 'express-adr-dependency-injection-typescript'");
 
 const rawCommands = fs.readdirSync(path.join(__dirname, 'commands'));
@@ -18,5 +22,14 @@ rawCommands.forEach((command) => {
     (require(path.join(__dirname, 'commands', command)).default)();
   } catch { }
 });
+
+try {
+  const latestVersion = execSync('npm show eadit-cli version').toString('utf8').trim().replace(/\r?\n|\r/g, '');
+  if (latestVersion && version) {
+    if (latestVersion !== version) {
+      console.log('\n\n', colors.yellow('WARNING'), colors.gray(':'), colors.white('You are not using the latest version!'), `${colors.gray('(')}Last: ${colors.green(latestVersion)}`, `Current: ${colors.red(version)}${colors.gray(')')}`, '\n\n');
+    }
+  }
+} catch { }
 
 program.parse(process.argv);
