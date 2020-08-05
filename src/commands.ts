96e51752ffa364f07fe336fa48ea5e8f21267b0c
RaresAil <ailincairares1@gmail.com>
2020-08-05 21:02:49 +0300

    Removed version checker

--- src/commands.ts
+++ src/commands.ts
@@ -3,13 +3,9 @@
 import program from 'commander';
 import fs from 'fs';
 import path from 'path';
-import colors from 'colors/safe';
-import { execSync } from 'child_process';
-
-const version = '1.0.3';
 
 program
-  .version(version)
+  .version('1.0.4')
   .description("CLI for 'express-adr-dependency-injection-typescript'");
 
 const rawCommands = fs.readdirSync(path.join(__dirname, 'commands'));
@@ -22,14 +18,5 @@ rawCommands.forEach((command) => {
     (require(path.join(__dirname, 'commands', command)).default)();
   } catch { }
 });
-
-try {
-  const latestVersion = execSync('npm show eadit-cli version').toString('utf8').trim().replace(/\r?\n|\r/g, '');
-  if (latestVersion && version) {
-    if (latestVersion !== version) {
-      console.log('\n\n', colors.yellow('WARNING'), colors.gray(':'), colors.white('You are not using the latest version!'), `${colors.gray('(')}Last: ${colors.green(latestVersion)}`, `Current: ${colors.red(version)}${colors.gray(')')}`, '\n\n');
-    }
-  }
-} catch { }
 
 program.parse(process.argv);
