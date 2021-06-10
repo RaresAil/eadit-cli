25ff007efad895d789bef97aae297ee4fcf46e83
RaresAil <ailincairares1@gmail.com>
2021-06-10 22:53:26 +0300

    feat(extra-tests)

--- src/__tests__/createProject/create.spec.ts
+++ src/__tests__/createProject/create.spec.ts
@@ -1,10 +1,11 @@
+import { exec } from 'child_process';
 import { expect } from 'chai';
 import Mocha from 'mocha';
 import { EOL } from 'os';
 import fs from 'fs';
 import path from 'path';
 
-import { execute, ENTER } from '../utils/cmd';
+import { execute, ENTER, SPACE } from '../utils/cmd';
 import questions from '../utils/questions';
 
 const clearProject = (done: Mocha.Done) => {
@@ -26,6 +27,7 @@ const clearProject = (done: Mocha.Done) => {
 
 describe('Create express project', () => {
   after(clearProject);
+  afterEach(clearProject);
 
   it('Without any deps', async () => {
     const response = (
@@ -52,5 +54,61 @@ describe('Create express project', () => {
       .pop();
 
     expect(response).to.contain('In the same folder with eaditconfig.json');
+
+    await new Promise((resolve, reject) => {
+      exec(
+        `cd "${path.join(process.cwd(), 'demo')}" && yarn test`,
+        (err, stdout) => {
+          if (err) {
+            return reject(err);
+          }
+
+          return resolve(stdout);
+        }
+      );
+    });
+  });
+
+  it('With 1 dep', async () => {
+    const response = (
+      (await execute(
+        'bin/commands.js',
+        ['create', 'demo'],
+        {
+          [questions.template]: ENTER,
+          [questions.path]: `Y ${ENTER}`,
+          [questions.yarn]: `Y ${ENTER}`,
+          [questions.deps]: `${SPACE}${ENTER}`,
+          [questions.clearDir]: `Y ${ENTER}`
+        },
+        {
+          env: {
+            DEBUG: true
+          }
+        }
+      )) as Promise<any>
+    )
+      .toString()
+      .trim()
+      .split(EOL)
+      .pop();
+
+    expect(response).to.contain('In the same folder with eaditconfig.json');
+    expect(
+      require(path.join(process.cwd(), 'demo', 'package.json')).dependencies
+    ).to.haveOwnProperty('morgan');
+
+    await new Promise((resolve, reject) => {
+      exec(
+        `cd "${path.join(process.cwd(), 'demo')}" && yarn test`,
+        (err, stdout) => {
+          if (err) {
+            return reject(err);
+          }
+
+          return resolve(stdout);
+        }
+      );
+    });
   });
 });
--- src/__tests__/utils/questions.ts
+++ src/__tests__/utils/questions.ts
@@ -3,5 +3,6 @@ export default {
   path: 'Is the path correct?',
   yarn: 'Yarn was detected on your system, do you want to use Yarn?',
   deps: 'What other dependencies do you want?',
+  mongoose: 'For Mongoose, please give a value for: MONGO_CONN_URL',
   clearDir: 'The current directory is not empty, do you want to clear it?'
 };
